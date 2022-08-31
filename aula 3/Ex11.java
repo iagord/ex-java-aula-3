37473be2f2c4bec7b013a873a37ad284e31116e3
iagord <rosadiasiago@gmail.com>
2022-08-30 22:02:42 -0300

    Ex11-e-Ex12

---
+++ aula 3/Ex11.java
@@ -0,0 +1,43 @@
+package ddd;
+
+import java.util.Scanner;
+
+public class Ex11 {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		
+		double a, v0, t, v;
+		
+		System.out.printf("Digite a aceleração: ");
+		a = ler.nextDouble();
+		
+		System.out.printf("Digite a velocidade inicial: ");
+		v0 = ler.nextDouble();
+		
+		System.out.printf("Digite o tempo de percurso: ");
+		t = ler.nextDouble();
+		
+		v = v0 + (a * t) / 3.6;
+		
+		if (v <= 40) {
+			System.out.printf("Veículo muito lento.");
+		}
+		else if ((v > 40) && (v <= 60)) {
+			System.out.printf("Velocidade permitida.");
+		}
+		else if ((v > 60) && (v <= 80)) {
+			System.out.printf("Velocidade de cruzeiro.");
+		}
+		else if ((v > 80) && (v <= 120)) {
+			System.out.printf("Veículo rápido.");
+		}
+		else {
+			System.out.printf("Veículo muito rápido.");
+		}
+		
+		ler.close();
+
+	}
+
+}
---
+++ aula 3/Ex12.java
@@ -0,0 +1,31 @@
+package ddd;
+
+import java.util.Scanner;
+
+public class Ex12 {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		
+		double p1, p2, m;
+		
+		System.out.printf("Digite sua p1: ");
+		p1 = ler.nextDouble();
+		
+		System.out.printf("Digite sua p2: ");
+		p2 = ler.nextDouble();
+		
+		m = (p1 + (2 * p2)) / 3;
+		
+		if (m >= 5) {
+			System.out.printf("Sua média foi %f e você foi Aprovado!");
+		}
+		else if (m < 5) {
+			System.out.printf("Sua média foi %f e você foi Reprovado.");
+		}
+		
+		ler.close();
+
+	}
+
+}
